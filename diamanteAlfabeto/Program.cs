a3ed81be7c714b7bb2fdfd038a767861c8703fb8
Jesiane Straubel <jesi1951@hotmail.com>
2023-03-28 13:06:16 -0300

    Add project files.

---
+++ diamanteAlfabeto/Program.cs
@@ -0,0 +1,82 @@
+﻿// control + k + D
+namespace diamanteAlfabeto
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("\r ** DIAMANTE DE X ** ");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            char letra = ' ';
+            bool letraValida = false;
+
+            while (!letraValida)
+            {
+                Console.Write("Insira uma letra maiúscula: ");
+                string input = Console.ReadLine();
+
+                if (char.TryParse(input, out letra) && char.IsUpper(letra))
+                {
+                    letraValida = true;
+                }
+                else
+                {
+                    Console.WriteLine("A letra inserida não é válida. Tente novamente.");
+                }
+            }
+
+            int numLetras = letra - 'A' + 1;
+
+            for (int i = 0; i < numLetras; i++)
+            {
+                for (int j = 0; j < numLetras - i - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.Write((char)('A' + i));
+
+                if (i > 0)
+                {
+                    for (int j = 0; j < i * 2 - 1; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    Console.Write((char)('A' + i));
+                }
+
+                Console.WriteLine();
+            }
+
+            for (int i = numLetras - 2; i >= 0; i--)
+            {
+                for (int j = 0; j < numLetras - i - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.Write((char)('A' + i));
+
+                if (i > 0)
+                {
+                    for (int j = 0; j < i * 2 - 1; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    Console.Write((char)('A' + i));
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
